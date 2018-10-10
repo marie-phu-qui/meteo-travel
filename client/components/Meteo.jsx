fab4bee215a16780b707ab22d02b4c14a87ad9d5
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-10-10 15:22:51 +1300

    form of bootstrap class

--- client/components/Meteo.jsx
+++ client/components/Meteo.jsx
@@ -18,11 +18,35 @@ render() {
     <p>
         Meteo
     </p>
-    <form>
-      <input className='user-input' type='text' name='unit' placeholder='unit'></input>
-      <input className='user-input' type='text' name='city' placeholder='city'></input>
-      <input className='btn' type='button' value='submit'></input>
+
+    
+    <div>
+
+    <form className="form-inline"> 
+    <label className="sr-only" htmlFor="inlineFormInput">city</label> 
+    <input className="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="City"/> 
+    <label className="sr-only" htmlFor="inlineFormInputGroup">country</label> 
+    
+    <div className="input-group mb-2 mr-sm-2 mb-sm-0"> 
+    <div className="input-group-addon">in
+    </div> 
+    <input className="form-control" id="inlineFormInputGroup" placeholder="Country"/>
+    </div> 
+
+    <div className="btn-group" data-toggle="buttons"> 
+    
+    <label className="btn btn-primary active"> 
+    <input type="radio" name="unit" value="metric" checked=""/>metric 
+    </label> 
+    <label className="btn btn-primary"> 
+    <input type="radio" name="unit" value="imperial"/> imperial 
+    </label> 
+
+    <input className='btn btn-primary btn-lg btn-block' type='button' value='submit'/>
+    </div>
     </form>
+    </div>
+
     <p>
       Your weather condition :
     </p>
